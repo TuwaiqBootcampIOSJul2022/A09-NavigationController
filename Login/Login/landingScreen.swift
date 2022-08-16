f2e29431751200fc22f4147233603ca266ff3dff
Razan Abdullah <razanabdullah@Razans-MacBook-Pro.local>
2022-08-16 09:17:53 +0300

    Done

---
+++ Login/Login/landingScreen.swift
@@ -0,0 +1,32 @@
+//
+//  landingScreen.swift
+//  Login
+//
+//  Created by Razan Abdullah on 17/01/1444 AH.
+//
+
+import UIKit
+
+class landingScreen: UIViewController {
+    
+    var UserN = " "
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = UserN
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
